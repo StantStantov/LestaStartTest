38b68a0330437e521edc850dd94a557bb0c84ba0
Imil <ibragimovimil0@gmail.com>
2025-04-09 19:51:57 +0300

    [feat] Add table creation

--- internal/routes.go
+++ internal/routes.go
@@ -5,7 +5,9 @@ import (
 	"bufio"
 	"log"
 	"maps"
+	"math"
 	"net/http"
+	"slices"
 )
 
 func HandleIndexGet() http.HandlerFunc {
@@ -25,20 +27,49 @@ func HandleIndexPost() http.HandlerFunc {
 		}
 		defer file.Close()
 
+		var totalAmount uint64 = 0
 		uniqueWords := map[string]uint64{}
 		scanner := bufio.NewScanner(file)
 		scanner.Split(bufio.ScanWords)
 		for scanner.Scan() {
 			word := scanner.Text()
-			uniqueWords[word] = uniqueWords[word] + 1
+			amount := uniqueWords[word] + 1
+			uniqueWords[word] = amount
+			totalAmount++
 		}
 		if err := scanner.Err(); err != nil {
 			log.Printf("Internal/routes.HandleIndexPost: [%v]", err)
 			return
 		}
 
+		table := make([]tfIdfTableRow, 0, totalAmount)
 		for word, amount := range maps.All(uniqueWords) {
-			log.Printf("%q: %d\n", word, amount)
+			table = append(table, tfIdfTableRow{word, amount, calculateIdf(totalAmount, amount)})
+		}
+		slices.SortFunc(table, compareRowsByIdf)
+
+		log.Printf("Total words amount: %d\n", totalAmount)
+		for i := range table {
+			log.Printf("%v", table[i])
 		}
 	})
+}
+
+type tfIdfTableRow struct {
+	word string
+	tf   uint64
+	idf  float64
+}
+
+func calculateIdf(wordsAmount uint64, wordAmount uint64) float64 {
+	return math.Log10(float64(wordsAmount) / float64(wordAmount))
+}
+
+func compareRowsByIdf(a tfIdfTableRow, b tfIdfTableRow) int {
+	if a.idf < b.idf {
+		return 1
+	} else if a.idf > b.idf {
+		return -1
+	}
+	return 0
 }
