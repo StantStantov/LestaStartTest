00b705b768a1d8e82619d8545822ca5007599a84
Imil <ibragimovimil0@gmail.com>
2025-05-28 17:30:00 +0300

    [feat] Add collectMetrics Middleware

---
+++ internal/api/web/middlewares/collectMetrics_test.go
@@ -0,0 +1,85 @@
+package middlewares_test
+
+import (
+	"Stant/LestaGamesInternship/internal/api/web/middlewares"
+	"Stant/LestaGamesInternship/internal/domain/stores"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestCollectMetrics(t *testing.T) {
+	// metricStore := stores.NewInMemoryMetricStore()
+	//
+	// metricMiddleware := middlewares.NewCollectMetricsMiddleware(metricStore)
+	//
+	// router := http.NewServeMux()
+	// router.Handle("POST /good", metricMiddleware(mockPostHandler(map[string]string{"Content-Type": "multipart/form-data"})))
+	// router.Handle("POST /bad", metricMiddleware(mockPostHandler(nil)))
+
+	t.Run("Collect if OK", func(t *testing.T) {
+		t.Helper()
+
+		wantLength := 2
+
+		metricStore := stores.NewInMemoryMetricStore()
+		router := newMockRouter(metricStore)
+		request, err := http.NewRequest(http.MethodPost, "/good", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		router.ServeHTTP(httptest.NewRecorder(), request)
+
+		metrics, err := metricStore.ReadAll()
+		if err != nil {
+			t.Fatal(err)
+		}
+		gotLength := len(metrics)
+
+		if wantLength != gotLength {
+			t.Errorf("Wanted %d, got %d", wantLength, gotLength)
+		}
+	})
+	t.Run("Do nothing if Error", func(t *testing.T) {
+		t.Helper()
+
+		wantLength := 0
+
+		metricStore := stores.NewInMemoryMetricStore()
+		router := newMockRouter(metricStore)
+		request, err := http.NewRequest(http.MethodPost, "/bad", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		router.ServeHTTP(httptest.NewRecorder(), request)
+
+		metrics, err := metricStore.ReadAll()
+		if err != nil {
+			t.Fatal(err)
+		}
+		gotLength := len(metrics)
+
+		if wantLength != gotLength {
+			t.Errorf("Wanted %d, got %d", wantLength, gotLength)
+		}
+	})
+}
+
+func newMockRouter(metricStore stores.MetricStore) *http.ServeMux {
+	metricMiddleware := middlewares.NewCollectMetricsMiddleware(metricStore)
+
+	router := http.NewServeMux()
+	router.Handle("POST /good", metricMiddleware(mockPostHandler(map[string]string{"Content-Type": "multipart/form-data"})))
+	router.Handle("POST /bad", metricMiddleware(mockPostHandler(nil)))
+
+	return router
+}
+
+func mockPostHandler(headers map[string]string) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		for key, value := range headers {
+			w.Header().Set(key, value)
+		}
+		w.WriteHeader(http.StatusOK)
+	})
+}
